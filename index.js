be555272659f83b8a07c5eae45e934b6012e28a6
Andy Thach <megaupgrade1@yahoo.com>
2018-11-08 22:49:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
-var greeting = "";
-
-var specialGuest = "Neil deGrasse Tyson"
-
-var greetSpecialGuest = "" + specialGuest + "!";
-
-var topic = "space";
-
-var conversation = `${topic}`;
+![lab](http://i.giphy.com/NETCsDYm0fL44.gif)
+ ## Objectives
+ 1. Manipulate strings by hand in JavaScript
+2. Practice interpolating with template literals
+ You'll find a file called `strings.js` in this directory. Your mission, should you choose to accept it, is to get tests to pass. You can run the tests using the `learn` command in your terminal or the Learn IDE. Give that a go now.
+ ![waiting](http://i.giphy.com/9c830567WqLCw.gif)
